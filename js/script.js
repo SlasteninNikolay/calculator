1db5fa86e9354b089b364bca4e6e251810ee5526
Slastenin Nikolay <slastenin13@gmail.com>
2021-08-02 23:43:34 +0300

    Fixed some mistakes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-let money = 60000,
-    income = "дивиденды",
-    addExpenses = "еда, проезд, коммуналка, бензин, интернет, телефон, кафе",
-    deposit = true,
-    mission = 800000,
-    period = 12,
-    budgetDay = money / 30,
-    expenses1,
-    expenses2,
-    amount1,
-    amount2,
-    budgetMonth;
+let money = +prompt("Ваш месячный доход?");
+let income = "депозит";
+let addExpenses = prompt(
+    "Перечислите возможные расходы за рассчитываемый период через запятую"
+);
+let deposit = confirm("Есть ли у вас депозит в банке?");
+let mission = 800000;
+let period = 12;
 
 console.log(typeof money);
 console.log(typeof income);
@@ -18,25 +14,21 @@ console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log("Период равен " + period + " месяцев");
 console.log("Цель заработать " + mission + " рублей");
+console.log(addExpenses.split(", "));
 
-money = prompt("Ваш месячный доход?");
-addExpenses = prompt(
-    "Перечислите возможные расходы за рассчитываемый период через запятую"
-).split(", ");
-console.log(addExpenses);
-deposit = confirm("Есть ли у вас депозит в банке?");
-expenses1 = prompt("Введите обязательную статью расходов?");
-amount1 = prompt("Во сколько это обойдется?");
-expenses2 = prompt("Введите обязательную статью расходов?");
-amount2 = prompt("Во сколько это обойдется?");
-budgetMonth = money - amount1 - amount2;
+let expenses1 = prompt("Введите обязательную статью расходов?");
+let amount1 = prompt("Во сколько это обойдется?");
+let expenses2 = prompt("Введите обязательную статью расходов?");
+let amount2 = prompt("Во сколько это обойдется?");
+
+let budgetMonth = money - amount1 - amount2;
 console.log("Бюджет на месяц составит: ", budgetMonth);
 console.log(
     "Цель будет достигнута за " +
         Math.ceil(mission / budgetMonth) +
         " месяцев(-а)"
 );
-budgetDay = budgetMonth / 30;
+let budgetDay = budgetMonth / 30;
 console.log("Бюджет на день составит: ", Math.floor(budgetDay));
 
 if (budgetDay >= 1200) {
